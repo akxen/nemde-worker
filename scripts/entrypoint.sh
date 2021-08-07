ced7c8f6f02b13c95d78678bdb91f1a6e3f0a48a
Aleksis Xenophon <aleksis.git@gmail.com>
2021-08-07 17:11:44 +0800

    (Update) Added env file template

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -9,9 +9,5 @@ export PATH=$PATH:/apps/nemde
 export PYTHONPATH=/apps/nemde
 echo $PATH
 
-# Initialise casefile database
-python3.9 /apps/nemde/scripts/initialise_tables.py
-python3.9 /apps/nemde/scripts/upload_casefiles.py
-
 # Start worker
 python3.9 /scripts/worker.py
